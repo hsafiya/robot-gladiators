f7b87b8dd5b596f60a56b88d0b89f9d0e991c35a
Safiyya H <safiyya.hasanova@gmail.com>
2021-05-16 20:54:02 -0500

    fight called out

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+var playerName = window.prompt("What is your robot's name?");
+
+
+window.alert("This is an alert! JavaScript is running");
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+fight();
